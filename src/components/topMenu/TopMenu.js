3f274d08a2d1462e454dad218d6dd39f123d1ca1
WolfOfFutility <ceejayfreelance@gmail.com>
2020-10-14 03:06:27 +1000

    Set up the menu design

--- src/components/topMenu/TopMenu.js
+++ src/components/topMenu/TopMenu.js
@@ -5,7 +5,12 @@ import "./TopMenu.css"
 const TopMenu = () => {
     return (
         <div className="top-menu-box">
-            Top Menu
+            <div className="logo-box">Personal Portfolio</div>
+            <div className="menu-button-box">
+                <div className="menu-button">Home</div>
+                <div className="menu-button">About</div>
+                <div className="menu-button">Projects</div>
+            </div>
         </div>
     )
 }
