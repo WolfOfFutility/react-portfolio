74d09521925f53b4b6df03fafe751e338296a8d3
WolfOfFutility <ceejayfreelance@gmail.com>
2020-10-14 10:47:30 +1000

    add functionality for blog components

--- src/components/blogList/Blog.js
+++ src/components/blogList/Blog.js
@@ -4,7 +4,11 @@ import "./BlogList.css"
 const Blog = (props) => {
     return (
         <div className="blog-box">
-            <div class="date-box">{props.date}</div>
+            <div className="date-box">{props.date}</div>
+            <div className="text-area-box">
+                <div className="title-box"><p>{props.title}</p></div>
+                <div className="author-box"><p>{props.author}</p></div>
+            </div>
             {/* <div class="title-box"></div>
             <div class="author-box"></div>
             <div class="description-box"></div> */}
--- src/components/blogList/BlogList.js
+++ src/components/blogList/BlogList.js
@@ -6,10 +6,8 @@ import Blog from "./Blog"
 const BlogList = () => {
     return (
         <div className="blog-list-box">
-            <Blog date="14 OCT" />
-            <Blog date="15 OCT" />
-            <Blog date="16 OCT" />
-            <Blog date="17 OCT" />
+            <Blog date="14 OCT" title="Starting this website" author="Ceejay Kelly" />
+            <Blog date="15 OCT" title="Setting up components" author="Ceejay Kelly" />
         </div>
     )
 }
